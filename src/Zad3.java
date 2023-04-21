41c33c269de969b0e7589b408c24db8e8b20bc49
Paulina <hanaczeq@gmail.com>
2023-04-21 11:13:32 +0200

    Utworzenie wzoru daty

--- src/Zad3.java
+++ src/Zad3.java
@@ -1,2 +1,17 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 public class Zad3 {
+        public static void main(String[] args) throws ParseException {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy ");
+
+            while (true) {
+
+
+            }
+        }
 }
