dc22e13d397d8b533f7e27d1b11b6c4921f833cc
Paulina <hanaczeq@gmail.com>
2023-04-21 11:14:19 +0200

    Utworzenie klasy Scanner i List

--- src/Zad3.java
+++ src/Zad3.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 public class Zad3 {
         public static void main(String[] args) throws ParseException {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy ");
-
+            List<Date> list = new ArrayList<Date>();
+            Scanner scanner = new Scanner(System.in);
             while (true) {
 
 
