bba3b4138f80780924aeca6b01fda07b6d559a77
piotrek <piotr.radecki00@gmail.com>
2023-04-22 10:10:17 +0200

    Dodanie pętli zewnętrznej i wewnętrznej

--- src/Zad1.java
+++ src/Zad1.java
@@ -9,5 +9,8 @@ public class Zad1 {
 
         int maxCellWidth = Integer.toString(n * n).length() + 1;
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+            }
+        }
     }
-}
