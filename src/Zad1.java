8c0a38bbd5b58fdb6f8b8e11febf64e46ae7ccb7
piotrek <piotr.radecki00@gmail.com>
2023-04-22 10:14:13 +0200

    Obliczenie wyniku mnożenia dwóch liczb oraz sformatowanie wyniku mnożenia

--- src/Zad1.java
+++ src/Zad1.java
@@ -11,6 +11,9 @@ public class Zad1 {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
+                int result = i * j;
+                String formattedResult = String.format("%-" + maxCellWidth + "d", result);
             }
         }
+    }
     }
