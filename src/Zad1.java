5104275e75d813f24e1856cd5c592863f84f6339
piotrek <piotr.radecki00@gmail.com>
2023-04-22 10:01:58 +0200

    Wyświetlenie tekstu z prośbą o podanie liczby naturalnej n

--- src/Zad1.java
+++ src/Zad1.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Zad1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbę naturalną n: ");
+        int n = scanner.nextInt();
+        scanner.close();
+    }
 }
