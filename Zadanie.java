8fe39ee3dd234633b212c7627cf24138d7cd3c40
piotrek <piotr.radecki00@gmail.com>
2023-05-07 23:40:18 +0200

    Definicja klasy abstrakcyjnej Zwierze

---
+++ Zadanie.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+public class Zadanie {
+
+    abstract class Zwierze {
+        String imie;
+        public Zwierze(String imie) {
+            this.imie = imie;
+        }
+    }
+}
+
