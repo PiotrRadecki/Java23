777ae72085aa632ea644f975fa5934ca5d94a30b
piotrek <piotr.radecki00@gmail.com>
2023-05-14 21:04:28 +0200

    Dodanie bibliotek i klasy Calculator

--- Zadanie3.java
+++ Zadanie3.java
@@ -1,2 +1,12 @@
-public class Zadanie3 {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Calculator extends JFrame implements ActionListener {
+    private JTextField display;
+    private JButton[] buttons;
+    private String[] buttonLabels = { "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+" };
+    private double firstNumber = 0.0;
+    private char operation = ' ';
+
 }
