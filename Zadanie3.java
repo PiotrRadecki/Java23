ba441c33ba12b4a54e466ed07ed81d7f99be3e75
piotrek <piotr.radecki00@gmail.com>
2023-05-14 21:06:54 +0200

    tworzymy interfejs użytkownika

--- Zadanie3.java
+++ Zadanie3.java
@@ -9,4 +9,28 @@ public class Calculator extends JFrame implements ActionListener {
     private double firstNumber = 0.0;
     private char operation = ' ';
 
+    public Calculator() {
+        setTitle("Calculator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        display = new JTextField();
+        display.setEditable(false);
+        add(display, BorderLayout.NORTH);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4, 4));
+
+        buttons = new JButton[16];
+        for (int i = 0; i < 16; i++) {
+            buttons[i] = new JButton(buttonLabels[i]);
+            buttons[i].addActionListener(this);
+            buttonPanel.add(buttons[i]);
+        }
+
+        add(buttonPanel, BorderLayout.CENTER);
+        pack();
+        setVisible(true);
+    }
+
 }
