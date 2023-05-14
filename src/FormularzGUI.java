1a9bb45bd68ff9841b5a435af90f6b1f2d2533ec
Paulina <hanaczeq@gmail.com>
2023-05-14 15:17:48 +0200

    Zmiana nazwy klasy na FormularzGUI.java

---
+++ src/FormularzGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FormularzGUI extends JFrame implements ActionListener {
+    private JTextField imieField, nazwiskoField, peselField;
+    private JButton submitButton;
+
+
+}
