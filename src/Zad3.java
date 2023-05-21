3c81e30dadeb0b8e7c0c68ec2dba52ab53e8c3e9
Paulina <hanaczeq@gmail.com>
2023-05-21 22:19:44 +0200

    Deklaracja pól i zmiana nazwy

---
+++ src/Zad3.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Zad3 extends JFrame {
+    private JTextField nicknameField;
+    private JPasswordField passwordField;
+    private JTextField firstNameField;
+    private JTextField lastNameField;
+    private JTextField emailField;
+    private JRadioButton maleRadioButton;
+    private JRadioButton femaleRadioButton;
