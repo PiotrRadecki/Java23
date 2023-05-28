d3d3560e4359a52deb50ffe42754e8fe641c62e0
piotrek <piotr.radecki00@gmail.com>
2023-05-28 21:17:09 +0200

    Dodanie measureAddTime i measureRemoveTime

--- src/Zad2.java
+++ src/Zad2.java
@@ -18,6 +18,26 @@ public class Zad2{
             System.out.println("ArrayList - Remove Time: " + arrayListRemoveTime + " ms");
             System.out.println("LinkedList - Add Time: " + linkedListAddTime + " ms");
             System.out.println("LinkedList - Remove Time: " + linkedListRemoveTime + " ms");
-
         }
+    private static long measureAddTime(List<Integer> list, int numElements) {
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < numElements; i++) {
+            list.add(i);
+        }
+
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
+
+    private static long measureRemoveTime(List<Integer> list) {
+        long startTime = System.currentTimeMillis();
+
+        while (!list.isEmpty()) {
+            list.remove(0);
+        }
+
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
 }
