3e7d804a9d3f501449b27cebf0306c956bff26fd
piotrek <piotr.radecki00@gmail.com>
2023-05-28 21:43:14 +0200

    Tworzenie ArrayListy i LinkedListy

--- src/Zad3.java
+++ src/Zad3.java
@@ -1,2 +1,10 @@
-public class Zad3 {
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Zad3{
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
 }
+
