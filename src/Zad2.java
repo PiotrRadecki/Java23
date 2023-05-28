c8663029403042e5f84baca556bf1f78660dc9eb
Paulina <hanaczeq@gmail.com>
2023-05-28 21:09:51 +0200

    Utworzenie Array listy

--- src/Zad2.java
+++ src/Zad2.java
@@ -1,2 +1,15 @@
-public class Zad2
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Zad2{
+        public static void main(String[] args) {
+            final int NUM_ELEMENTS = 1000000;
+
+            // ArrayList
+            List<Integer> arrayList = new ArrayList<>();
+            long arrayListAddTime = measureAddTime(arrayList, NUM_ELEMENTS);
+            long arrayListRemoveTime = measureRemoveTime(arrayList);
+
+        }
 }
