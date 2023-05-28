8ed6ba857ec99d9eb29c5216643efc1fbca8426b
piotrek <piotr.radecki00@gmail.com>
2023-05-28 21:46:45 +0200

    Dodanie elementów do LinkedListy + czas

--- src/Zad3.java
+++ src/Zad3.java
@@ -14,5 +14,14 @@ public class Zad3{
         long endTime = System.nanoTime();
         long arrayListAddTime = endTime - startTime;
         System.out.println("Czas dodawania 10 000 elementów na początek ArrayList: " + arrayListAddTime + " ns");
-}
+        // Dodawanie 10 000 elementów na początek listy - LinkedList
+        startTime = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            linkedList.addFirst(i);
+        }
+        endTime = System.nanoTime();
+        long linkedListAddTime = endTime - startTime;
+        System.out.println("Czas dodawania 10 000 elementów na początek LinkedList: " + linkedListAddTime + " ns");
+
+    }
 
