dab6d4333b1a33a0746c5fe931c071021b93a859
Paulina <hanaczeq@gmail.com>
2023-05-28 21:11:00 +0200

    Wyświetlenie zawartości

--- src/Zad2.java
+++ src/Zad2.java
@@ -6,16 +6,18 @@ public class Zad2{
         public static void main(String[] args) {
             final int NUM_ELEMENTS = 1000000;
 
-            // ArrayList
             List<Integer> arrayList = new ArrayList<>();
             long arrayListAddTime = measureAddTime(arrayList, NUM_ELEMENTS);
             long arrayListRemoveTime = measureRemoveTime(arrayList);
-            // LinkedList
+
             List<Integer> linkedList = new LinkedList<>();
             long linkedListAddTime = measureAddTime(linkedList, NUM_ELEMENTS);
             long linkedListRemoveTime = measureRemoveTime(linkedList);
 
-
+            System.out.println("ArrayList - Add Time: " + arrayListAddTime + " ms");
+            System.out.println("ArrayList - Remove Time: " + arrayListRemoveTime + " ms");
+            System.out.println("LinkedList - Add Time: " + linkedListAddTime + " ms");
+            System.out.println("LinkedList - Remove Time: " + linkedListRemoveTime + " ms");
 
         }
 }
