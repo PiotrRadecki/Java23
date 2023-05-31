0e75745366551c1d9e2a9a8d7a2bf541beb7fe9b
Paulina <hanaczeq@gmail.com>
2023-05-31 13:23:37 +0200

    Tworzenie wątków

--- src/Zad2.java
+++ src/Zad2.java
@@ -14,6 +14,18 @@ public class Zad2 {
     public static void main(String[] args) {
         Counter counter = new Counter();
 
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.increment();
+            }
+        });
+
 
     }
 }
