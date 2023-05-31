edbcaccf31dd51dd39bb0263d690f3f48e8ee2b9
piotrek <piotr.radecki00@gmail.com>
2023-05-31 13:33:49 +0200

    Tworzenie okna i dane

--- src/Zad3.java
+++ src/Zad3.java
@@ -1,1 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Zad3 extends JFrame {
+
+    private JButton noThreadButton;
+    private JButton withThreadButton;
+    private JLabel statusLabel;
+
+    public Zad3() {
+        setTitle("Porównanie wersji");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 200);
+        setLayout(new FlowLayout());
+
+        noThreadButton = new JButton("Bez wątków");
+        withThreadButton = new JButton("Z wątkami");
+        statusLabel = new JLabel("Status");
+
+        add(noThreadButton);
+        add(withThreadButton);
+        add(statusLabel);
+
+    }
+}
 
